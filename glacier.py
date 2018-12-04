93a16c022d96a951745bceba7908c84c2fa0e5f5
TTKettler <Tosca_esther@hotmail.com>
2018-12-04 11:22:31 +0100

    Revert "Merge branch 'master' of https://github.com/jsprdejong/Ice-ClimateProject"
    This reverts commit 5d325e1d85aa582831d4f3a57c5ff1ccef301cad, reversing
    changes made to 66c997b7f8d0f1ee1ea0d3bf055c88b49a59a0be.

--- glacier.py
+++ glacier.py
@@ -7,10 +7,8 @@ import numpy as np #This file is a preliminary version of the ice sheet model
 import matplotlib.pyplot as plt
 
 #set variables
-Bs = 0 # Total surface mass budget
-F = 0 # Calving flux (<0)
 b0 = 3900 #m
-s=0.1 # Bed slope
+s=0.1
 Lmax = b0/s
 dx = 100. #m
 beta=0.007 #m ice/a/m
@@ -19,23 +17,16 @@ nu=10
 E0=2900 # m height equilibrium line t=0
 dt=1 #yr
 
-ds_dl = 0
 
 #set bottom slope glacier
 x_arr = np.arange(0,Lmax,dx)
 
-def mean_ice_thickness(alpha,nu,mean_s,L):
-    return alpha/(1.+ nu*mean_s) * np.sqrt(L)
-
 def bed_slope(x):
     return b0 - s * x
-
-def dL_dt(L,alpha,nu,s,Bs,F):
-    return (3*alpha/(2*(1+nu*s))*L**(1/2) \
-    -alpha*nu/(1+nu*s)**2*L**(3/2)*ds_dl)**(-1)\
-    *(Bs+F)        
 
 # =============================================================================
 # Main programm
 # =============================================================================
 b_arr = bed_slope(x_arr)
+
+print(dt)
