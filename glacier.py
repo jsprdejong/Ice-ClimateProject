a4b1af58cec3cf6205ac4eca529a8fe01e5ec407
TTKettler <Tosca_esther@hotmail.com>
2018-12-04 10:20:04 +0100

    set variables section

--- glacier.py
+++ glacier.py
@@ -1,2 +1,11 @@
-import numpy as np %This file is a preliminary version of the ice sheet model
+import numpy as np #This file is a preliminary version of the ice sheet model
 import matplotlib.pyplot as plt
+
+set variables
+b0=3900 #m
+s=0.1
+beta=0.007 #m ice/a/m
+alpha=3 #m^0.5
+nu=10
+E0=2900 # m height equilibrium line t=0
+dt=1 #yr
