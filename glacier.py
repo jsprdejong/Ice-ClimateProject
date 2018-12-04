d3307b1990346ce31760a53529ee54e7d601f2dc
TTKettler <Tosca_esther@hotmail.com>
2018-12-04 10:45:18 +0100

    Revert "Update glacier.py"
    This reverts commit 424491325c03ae969bef01d1127e4015743cab0f.

--- glacier.py
+++ glacier.py
@@ -11,8 +11,8 @@ E0=2900 # m height equilibrium line t=0
 dt=1 #yr
 
 #set bottom slope glacier
-x=np.zeros(b0/s)
-b=np.zeros(x)
+x=np.zeros(len(b0/s))
+b=np.zeros(len(x))
 
 #bottom bed slope
 for i in range(len(x)):
