07b2a883fe0c9e38957c818a6b245ad433d02ad2
TTKettler <Tosca_esther@hotmail.com>
2018-12-04 10:23:49 +0100

    Update glacier.py

--- glacier.py
+++ glacier.py
@@ -1,7 +1,7 @@
 import numpy as np #This file is a preliminary version of the ice sheet model
 import matplotlib.pyplot as plt
 
-set variables
+#set variables
 b0=3900 #m
 s=0.1
 beta=0.007 #m ice/a/m
@@ -9,3 +9,11 @@ alpha=3 #m^0.5
 nu=10
 E0=2900 # m height equilibrium line t=0
 dt=1 #yr
+
+#set bottom slope glacier
+x=np.zeros(len(b0/s))
+b=np.zeros(len(x))
+
+#bottom bed slope
+for i in range(len(x)):
+    b[i]=b0-sx
