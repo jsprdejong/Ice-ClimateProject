dddff4d0ee6798abda7a144202faa94dc8e4a488
paul <paul.petersik@gmail.com>
2018-12-04 10:40:45 +0100

    using __future__

--- glacier.py
+++ glacier.py
@@ -1,19 +1,32 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
 import numpy as np #This file is a preliminary version of the ice sheet model
 import matplotlib.pyplot as plt
 
 #set variables
-b0=3900 #m
+b0 = 3900 #m
 s=0.1
+Lmax = b0/s
+dx = 100. #m
 beta=0.007 #m ice/a/m
 alpha=3 #m^0.5
 nu=10
 E0=2900 # m height equilibrium line t=0
 dt=1 #yr
 
-#set bottom slope glacier
-x=np.zeros(len(b0/s))
-b=np.zeros(len(x))
 
-#bottom bed slope
-for i in range(len(x)):
-    b[i]=b0-sx
+#set bottom slope glacier
+x_arr = np.arange(0,Lmax,dx)
+
+def bed_slope(x):
+    return b0 - s * x
+
+# =============================================================================
+# Main programm
+# =============================================================================
+b_arr = bed_slope(x_arr)
+
+print(dt)
