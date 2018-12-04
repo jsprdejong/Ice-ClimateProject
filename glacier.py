d563cdc71c115cc0fb37753889c728bce8aa5071
jsprdejong <43541913+jsprdejong@users.noreply.github.com>
2018-12-04 11:36:02 +0100

    Update glacier.py

--- glacier.py
+++ glacier.py
@@ -18,6 +18,8 @@ alpha=3 #m^0.5
 nu=10
 E0=2900 # m height equilibrium line t=0
 dt=1 #yr
+bed_profile = 'linear' # or use 'concave'
+x_l = 1000 #m length scale where bed profile drops off
 
 ds_dl = 0
 
@@ -33,9 +35,16 @@ def bed_slope(x):
 def dL_dt(L,alpha,nu,s,Bs,F):
     return (3*alpha/(2*(1+nu*s))*L**(1/2) \
     -alpha*nu/(1+nu*s)**2*L**(3/2)*ds_dl)**(-1)\
-    *(Bs+F)        
+    *(Bs+F)    
+
+def ds_dL(s,b0,L,x,b_l):   
+    if bed_profile == 'linear':
+        return 0
+    elif bed_profile == 'concave':
+        return -b0(1-exp(-L/x_l))/L**2 + b0*x_l**(-1)/L
 
 # =============================================================================
 # Main programm
 # =============================================================================
 b_arr = bed_slope(x_arr)
+print ds_dL(s,b0,L,x,b_l)
