bae79e1b8568f7004e0f69d6bb777104b3645d1e
paul <paul.petersik@gmail.com>
2018-12-04 11:20:52 +0100

    mean_ice_thickness function

--- glacier.py
+++ glacier.py
@@ -19,22 +19,23 @@ nu=10
 E0=2900 # m height equilibrium line t=0
 dt=1 #yr
 
+ds_dl = 0
 
 #set bottom slope glacier
 x_arr = np.arange(0,Lmax,dx)
+
+def mean_ice_thickness(alpha,nu,mean_s,L):
+    return alpha/(1.+ nu*mean_s) * np.sqrt(L)
 
 def bed_slope(x):
     return b0 - s * x
 
 def dL_dt(L,alpha,nu,s,Bs,F):
-    ds_dl = 0
-    return (3*alpha/(2*(1+nu*s))*L^(1/2) \
-    -alpha*nu/(1+nu*s)^2*L^(3/2)*ds_dl)^(-1)\
+    return (3*alpha/(2*(1+nu*s))*L**(1/2) \
+    -alpha*nu/(1+nu*s)**2*L**(3/2)*ds_dl)**(-1)\
     *(Bs+F)        
 
 # =============================================================================
 # Main programm
 # =============================================================================
 b_arr = bed_slope(x_arr)
-
-print(dt)
